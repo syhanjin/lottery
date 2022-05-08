3d056c70c72b590b48f1ea2b68fb5be35c4fdc07
syhanjin <2819469337@qq.com>
2022-05-08 11:59:48 +0800

    0.2.4
    微调

--- builder.bat
+++ builder.bat
@@ -1,12 +1,3 @@
 @echo off
 
-conda activate lottery
-
-PyInstaller -D console.spec
-PyInstaller -D main.spec
-
-mkdir .\dist\lottery
-xcopy ".\dist\console\*.*" ".\dist\lottery" /e /y
-xcopy ".\dist\main\*.*" ".\dist\lottery" /e /y
-
-rd __pycache__ build dist\console dist\main /q/s
+conda activate lottery&&PyInstaller -D console.spec&&PyInstaller -D main.spec&&mkdir .\dist\lottery&&xcopy ".\dist\console\*.*" ".\dist\lottery" /e /y&&xcopy ".\dist\main\*.*" ".\dist\lottery" /e /y&&rd __pycache__ build dist\console dist\main /q/s
